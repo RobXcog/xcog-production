765acd998bc8086f63c7da39ac2b7becd7ea342d
Rob Xcog <rob@xcogstudios.com>
2014-12-24 14:18:48 -0800

    updating alpha 0.0.a

--- frontend/views/xcog/robxcog.php
+++ frontend/views/xcog/robxcog.php
@@ -38,7 +38,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <div id="robsJumbo">
        
         <div class="row">
-          <img class="xcogRobBanner"src="https://farm4.staticflickr.com/3934/15452349119_7b7ae7a7c7_o.jpg" alt="robXcog#cosmicgeometry">
+       
           <span class="col-sm-5 col-sm-offset-1">  
     
               <div class="xcogWarning text-center" > <h2 class="text-danger">Warning!</h2> Juicy and Personal Textual Content by Rob Xcog </span>
@@ -55,9 +55,9 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
     <div>
     <p class="dark-text">
-        <i class="fa fa-quote-left fa-3x pull-left fa-border"></i> <strong>Welcome to my (over)exposure, I am indulging in myself here and I am going to have fun making me look so totally awesome. I can do anything here for you too, I hope to share templates and DIY stuff here soon.
+        <i class="fa fa-quote-left fa-3x pull-left fa-border"></i> <strong>Welcome to my (over)exposure, I am indulging in myself here and I am going to have fun making myself look totally awesome. Welcome to my little place on the web. Please make yousrself at home.
             <br>
-            I am strong defender of acceptance and benevolent practices. I just do'nt act like it, and my art is very dark at times, so please be<span class="text-danger"> warned </span>! ! </strong> I also want to mix in useful data amongst the "aRt",  meaning this, please take my satyr with a grain of salt. or two. but less than 12357 mg/kg. :) 
+            I am <span class="text-danger"> warned </span>! ! </strong> I also want to mix in useful data amongst the "aRt",  meaning this, please take my satyr with a grain of salt. or two. but less than 12357 mg/kg. :) 
     </p>
     
 
@@ -65,30 +65,28 @@ $this->params['breadcrumbs'][] = $this->title;
     <hr>
     <span class="fa-border pull-left SIC symbolist">.symbolist</span>     <h2>$this->me.parse(<a type="link" class="text-justify" data-toggle="modal" data-target="#rachet">#SELFIE</a>);</h2>
     <hr>
-   I do not like to talk about myself much but I do like to talk about what I think about, a lot. I am not smart but I am systematic and tenacious in my discovery and design ethics. I have been labeled a renaissance man and while flattered I am wary labels and the strange power they can hold over us.
-   <br>
-   First and foremost I accept that I am a human and I am bound by human nature or as I would describe it, <strong>our innate capacity</strong>. The short story is that I trust us all to be, well.. us and I find our decisions fascinating. I was molded by tragedy in my life and I have been taken advantaged of for being too nice, I learned early that no act of kindness goes unpunished. Nieve and sheltered was my youth, I exploded into the world and got caught up in "game". 
-   <br>My life was strife, made from my own choices so I chose to solve them and get what I could from every moment. I am an introvert and a humanitarian.  I have a stability about me and my thoughts my assumptions have good reasons and if they don't they wont sway my opinoin. I do this through consistant(ish) introspection and self discipline exercises. 
+   I do not like to talk about myself however I do like to talk about what I want to do or how we can do things together. If you havent noticed by now I am a bit of a " metal head " however I respect all music and I respect talent and dedication. I enjoy all music and make it a point to judge all things slowly.  
+    <br>My life was loss and strife for almost half of it, a life grown from my own choices so I had to make the choice to make my life the way I want it. I became a fire-fighter for a short time and made new friends. Then I taught myself a handful of programming languages and now I find myself here. Writing to the faceless users my life is so dedicated to serving. from every moment. I am an introvert but I like to host. I am a philanthropist at heart but it is not a title I can say I have earned yet.I love music and animation. I love math and I am enthralled with (usually human)psychology, with a focus on emotion (deception detection). 
    <hr>
-   I hope to share on here and over time who I am, my passions, my fears, my rage and my tears with any and all who may be interested. I will gradually put out my story here that intertwines with other stories even your's since your reading this. . I will state thoughts as I like things to be and as I think things really are and not hopefully how I fear they might. I have been dubbed with a few happy mental disorders (who hasn't now adays right!? heh.. hey. did you just point at me??) anyways when it comes to singleminded focus I can be oblivious to flash mob strip tease in a crowded church. my peers have told me I have problems, I think they just be hating cuz I play guitar and Im a pretty guy. Everyone hates pretty dudes, it's my curse. I digress from my speech of uwavering focus.. wait. nevermind. I have been sitting at this computer withiin 40 yards of this spot for about a year now, and I finally taught myself how to build a website from the gorund up. Also to boot I achieved basic understanding of programming patterns, 9 programming syntaxes, digital-audio production and distribution, image manipulation and color theories, aesthetic sciences, business strategies and emotional and economic psychology, basic trigonometry and differential calculus with some statistics. I am far from what I would call myself comfortable in one of those broad disciplines but I have made a solution for every problem I have faced. <br>
+   I hope to share over time, who I am, my passions, my fears, my rage .. my tears. <br>
    <hr>
    Thank you for your interest in me and my work. I hope to bring some quality projects to the public soon and I encourage input and critiques from across the whole board. I am always happy to chat about the future. 
    
-   though it takes a lot of effort on my part to focus on what I should over what I want. The doctors call it ADD, I call it I have something more interesting available to me. when I feel I am correct though I practice an open mind and know very well that even though we may use the same words, they can represent subtly different thoughts than opinion that   of from what I have come to accept as part of  I am a designer, I love to doodle and play guitar. I am an enthusiast by nature, with a personal pursuit to discover (my) life's mysteries as well as enjoy the ride. I hope to find how I can actualize my potentials or even what my potentials could or should be. I am not classically trained or educated and I've hacked my way through life in many regards but I am tenacious with my focuses and humble with my criticisms (unless I think it's funny to not be, heh ). I have taught myself what I know by reading, watching and experimenting. I, like many, have built upon the shoulders of those who came before me to go farther than I alone could. I do hope that others will build with me, so that we can all go farther than we may-could alone. I hope to provide a launching pad for a few projects here some very community driven with a positive social and economic impact for individuals that may slip through societies fickle views and I hope to share my passions in a valuable way, even my rather dark ones. Out of it all I hope to contribute a positive influence for anyone of mind. I am pro passion, and pro human and I appreciate your attention. Thank You. 
+    I appreciate your attention. Thank You. 
     </p>
     <p class="body-content">
-        <a href=" <?= yii\helpers\Url::toRoute(['xcog/resume'],['breadcrumbs'=>['label'=>'Rob Xcog', 'url' => 'xcog/resume']]) ?>">Résumé</a> and my <a href="/robxcog/robsinteractiveresume">blog</a>. I also will be posting my attempts at art, music and logic. Soon. Very Soon. \m/ 
+        <a href=" <?= yii\helpers\Url::toRoute(['xcog/resume'],['breadcrumbs'=>['label'=>'Rob Xcog', 'url' => 'xcog/resume']]) ?>">Résumé</a> and my <a href="/robxcog/robintheblog">blog</a>. I also will be posting my attempts at art, music and logic. Soon. Very Soon. \m/ 
     </p>
     <p>
-        <?php yii\helpers\Url::to(['xcog/resume']) ?>
+        <?php yii\helpers\Url::to(['xcog/resume'],['breadcrumbs'=>['label'=>'Rob Xcog', 'url' => 'xcog/resume']]) ?>
     </p>
-    </div>
+
    
     
     
     
     
-    
+    </div>
 </div>
 <!-- Modal -->
 <div class="modal fade bs-example-modal-lg" id="rachet" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -99,50 +97,10 @@ $this->params['breadcrumbs'][] = $this->title;
         <h4 class="modal-title" id="myModalLabel">So Ratchet . . </h4>
       </div>
       <div class="modal-body">
-           <script>
-      // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
 
-      tag.src = "//www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-         
-          width: '100%',
-          videoId: 'kdemFfbS5H0',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-    </script>
+          <iframe width="100%" height="800px" src="//www.youtube.com/embed/kdemFfbS5H0" frameborder="0" allowfullscreen></iframe>
     
-        <div id="player"></div>
+      
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Drop</button>
@@ -151,3 +109,5 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
   </div>
 </div>
+
+
